bd7607d6e6a0a6401711f0aaf786438b18f621ce
lucia <llcsaturnaliam@gmail.com>
2024-01-01 15:28:14 -0600

    added more comments!

--- src/include/hacks.cpp
+++ src/include/hacks.cpp
@@ -12,18 +12,38 @@ void hacks::disableCoinsCap(bool hackEnabled) {
   if (!hackEnabled) hacks::setCoins(MAX_COINS);
 }
 
+/**
+ * \brief Sets coins to a specific value.
+ *
+ * \param coins The value to set the coins to.
+ */
 void hacks::setCoins(std::int32_t coins) {
   *global::game.coinAddress = (coins / 10);
 }
 
+/**
+ * \brief Sets the amount of chocolate to a specific value.
+ *
+ * \param chocolate The value to set the amount of chocolate to.
+ */
 void hacks::setChocolate(std::int32_t chocolate) {
   *global::game.chocolateAddress = (chocolate + 1000);
 }
 
+/**
+ * \brief Sets the amount of bug spray to a specific value.
+ *
+ * \param bugSpray The value to set the amount of bug spray to.
+ */
 void hacks::setBugSpray(std::int32_t bugSpray) {
   *global::game.bugSprayAddress = (bugSpray + 1000);
 }
 
+/**
+ * \brief Sets the amount of fertilizer to a specific value.
+ *
+ * \param fertilizer The value to set the amount of fertilizer to.
+ */
 void hacks::setFertilizer(std::int32_t fertilizer) {
-
+  *global::game.fertilizerAddress = (fertilizer + 1000);
 }
